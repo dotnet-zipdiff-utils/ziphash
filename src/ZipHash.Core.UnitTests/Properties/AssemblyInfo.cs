0ed1e95c358de9b8dead08bb6047b9635b8eaf4e
leekelleher <leekelleher@gmail.com>
2013-11-04 18:02:31 +0000

    Added stub proj for unit-tests

---
+++ src/ZipHash.Core.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ZipHash.Core.UnitTests")]
+[assembly: AssemblyConfiguration("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("B7C86A8F-C4A0-462B-B320-5AA1F1C15158")]
